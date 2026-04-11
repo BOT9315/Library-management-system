bd99572bc3a91538001c6e473db111c8e2bbe8f9
Ankush <akrk053@gmail.com>
2026-04-11 18:28:17 +0530

    Improve input handling in issue_books function
    Updated input handling for issuing books by stripping whitespace.

--- issue_books.py
+++ issue_books.py
@@ -2,7 +2,7 @@ from utils import books, issued_books
 
 def issue_books():
     if not books:
-        print("No books are available for issue,sorry for your inconvenience. Please check back later.")
+        print("No books are available for issue, sorry for your inconvenience. Please check back later.")
         return
     else:
         print("\nBooks available for issue are:")
@@ -10,7 +10,7 @@ def issue_books():
     for book_id, book in books.items():
         print(f"{book_id}. {book['title']}")
 
-    choice = input("Enter book number to issue from library: ").title()
+    choice = input("Enter book number to issue from library: ").strip() 
 
     if choice.isdigit():
         choice = int(choice)
@@ -20,6 +20,5 @@ def issue_books():
             print("Book issued! Please return it within 14 days.")
         else:
             print("Invalid number or book already issued.")
-        
     else:
         print("Invalid input. Please enter a valid book number.")
