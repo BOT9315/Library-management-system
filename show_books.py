51b697dd7ec53181e05bff0e1fdcd2e19bda2319
Ankush <akrk053@gmail.com>
2026-04-24 20:27:11 +0530

    Refactor show_books function for readability

--- show_books.py
+++ show_books.py
@@ -3,6 +3,7 @@ from utils import books
 def show_books():
     if not books:
         print("No books in the library,sorry for your inconvenience. Please check back later.")
+        
     else:
         print("\nBooks in the library are present:")
         for book_id, book in books.items():
